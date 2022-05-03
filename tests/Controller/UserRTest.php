e416e298f9fae9833a8fa9e789633e3b6813dbd2
Allan BIVILLE <71440994+AllanBiville@users.noreply.github.com>
2022-05-03 10:45:21 +0200

    Update and rename UserTest.php to UserRTest.php

--- tests/Controller/UserRTest.php
+++ tests/Controller/UserRTest.php
@@ -6,7 +6,7 @@ use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
-class UserTest extends WebTestCase
+class UserRTest extends WebTestCase
 {
     public function loginSuperadmin($username = 'superadmin', $role = 'ROLE_SUPER_ADMIN')
     {
